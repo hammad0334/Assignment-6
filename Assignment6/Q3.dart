2beeb1947482fec9f75840a422240a75a6b54e33
Hammad <0331hammad@gmail.com>
2025-08-08 05:13:47 -0700

    Q3 file created

--- Assignment6/Q3.dart
+++ Assignment6/Q3.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
 void main() {
+  stdout.write('Enter a number');
+  int number = int.parse(stdin.readLineSync()!);
+
+print('\nMutiplication table of $number:');
+for (int i = 1; i <= 10; i++) {
+  print('$number x $i = ${number * i}');
+}
               
 }
